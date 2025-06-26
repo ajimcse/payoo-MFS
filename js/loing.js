2d15af29ec9a946a6c46acef7da5252ec5d75c7d
ajimcse <mdajimsorkarcse40@gmail.com>
2025-06-26 09:22:21 +0600

    second commeit

---
+++ js/loing.js
@@ -0,0 +1,8 @@
+// console.log('click me ');
+document.getElementById('loing-btn').addEventListener('click', function(event){
+    event.preventDefault()
+    const inputNumber=document.getElementById('input-number').value
+    console.log(inputNumber);
+ 
+
+})
